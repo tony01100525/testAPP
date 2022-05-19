0797d2fe4938363b8a166e7c706ee4496a9b0f5b
tony01100525 <51709309+tony01100525@users.noreply.github.com>
2022-05-19 22:05:36 +0800

    Add files via upload

--- studio/quizConfig.js
+++ studio/quizConfig.js
@@ -1,7 +1,7 @@
 export default {
   localWebHost: 'http://localhost:3000', // default Vue port
   localApiHost: 'http://localhost:3000', // default Now API port
-  remoteWebHost: 'http://tony.vercel.app',
+  remoteWebHost: 'https://test-app-tony01100525.vercel.app',
   match: {
     correctAnswerScore: 100,
     firstAnswerScore: 50
